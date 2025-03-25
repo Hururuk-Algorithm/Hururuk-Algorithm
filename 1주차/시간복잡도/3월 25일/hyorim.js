2480d2a74f113581cf12155df38cebcbdcd6da59
hyorimhan <mynameishan8@gmail.com>
2025-03-25 19:16:31 +0900

    배열 정렬

--- 1주차/시간복잡도/3월 25일/hyorim.js
+++ 1주차/시간복잡도/3월 25일/hyorim.js
@@ -1,5 +1,4 @@
 /**
-<<<<<<< HEAD
  * 정렬 알고리즘 문제
  *
  * 문제 설명:
@@ -11,22 +10,13 @@
  */
 
 function solution(numbers) {
-  const numbers1 = [1, -5, 2, 4, 3];
-  return;
-=======
- 
-정렬 알고리즘 문제
-문제 설명:
-주어진 정수 배열을 오름차순으로 정렬하는 함수를 구현하세요.
-제약 조건:
-배열의 길이는 2 이상 10^5 이하입니다.
-배열의 각 원소는 -100,000부터 100,000 사이의 정수입니다.
-*/
-
-function solution(numbers) {
-  // 여기에 코드를 작성하세요
->>>>>>> afff4894c631e292d76805be6f08cd96956dbfb6
+  const sortedNumbers = numbers.sort((a, b) => a - b);
+  console.log(sortedNumbers);
 }
+
+solution([1, -5, 2, 4, 3]);
+solution([2, 1, 1, 3, 2, 5, 4]);
+solution([6, 1, 7]);
 
 // 예시:
 // 입력: [1, -5, 2, 4, 3]
