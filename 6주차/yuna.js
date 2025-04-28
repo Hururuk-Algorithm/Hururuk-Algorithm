08f80f3bddab487b5be7df7ea5ea95a5d0e8c001
yuna-hh <ghddbsk12@naver.com>
2025-04-28 15:59:29 +0900

    4/28 코드카타 완료

--- 6주차/yuna.js
+++ 6주차/yuna.js
@@ -1,1 +1,48 @@
-//
+// 문제 : 프로그래머스 - 전화번호 목록
+
+// 문제 설명
+
+/*
+전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
+전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
+
+구조대 : 119
+박준영 : 97 674 223
+지영석 : 11 9552 4421
+전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때, 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+*/
+
+// 제약 조건
+// phone_book의 길이는 1 이상 1,000,000 이하입니다.
+// 각 전화번호의 길이는 1 이상 20 이하입니다.
+// 같은 전화번호가 중복해서 들어있지 않습니다.
+
+// 입출력 예시
+// | phone_book | return |
+// |----------|--------|
+// | ["119", "97674223", "1195524421"] | false |
+// | ["123","456","789"] | true |
+// | ["12","123","1235","567","88"] | false |
+
+// 시간복잡도 O(n²)
+function solution(phone_book) {
+  let arr = [];
+  for (let item of phone_book) {
+    arr = phone_book.filter((el) => el.startsWith(item));
+    if (arr.length > 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// 시간복잡도 O(n log n)
+function solution(phone_book) {
+  phone_book.sort();
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].startsWith(phone_book[i])) {
+      return false;
+    }
+  }
+  return true;
+}
