37c1370f4ae222f3580c78030b3e4603433e8f6a
hyorimhan <mynameishan8@gmail.com>
2025-04-28 15:18:54 +0900

    .

--- 6주차/hyorim.js
+++ 6주차/hyorim.js
@@ -1,1 +1,59 @@
 //
+function solution(hp) {
+  var 장군 = Math.floor(hp / 5);
+  hp %= 5;
+  var 병정 = Math.floor(hp / 3);
+  hp %= 3;
+
+  const worker = hp;
+
+  return 장군 + 병정 + worker;
+}
+
+// 모스부호
+function solution(letter) {
+  const morse = {
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+  };
+
+  var answer = letter
+    .split(' ')
+    .map((code) => morse[code])
+    .join('');
+  return answer;
+}
+
+//
+function solution(rsp) {
+  const win = { 2: '0', 0: '5', 5: '2' };
+  var answer = rsp
+    .split('')
+    .map((s) => win[s])
+    .join('');
+  return answer;
+}
